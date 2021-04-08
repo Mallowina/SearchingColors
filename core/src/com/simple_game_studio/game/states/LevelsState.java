3cdde3981c7b8aa367f192c2d1221b91c75c6b30
Mallowina <2003ksenia@mail.ru>
2021-04-08 19:45:18 +0300

    add all states

---
+++ core/src/com/simple_game_studio/game/states/LevelsState.java
@@ -0,0 +1,31 @@
+package com.simple_game_studio.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.simple_game_studio.game.tools.GameStateManager;
+import com.simple_game_studio.game.tools.State;
+
+public class LevelsState extends State {
+    public LevelsState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/simple_game_studio/game/states/levels/Level_5_State.java
@@ -0,0 +1,31 @@
+package com.simple_game_studio.game.states.levels;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.simple_game_studio.game.tools.GameStateManager;
+import com.simple_game_studio.game.tools.State;
+
+public class Level_5_State extends State {
+    public Level_5_State(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
